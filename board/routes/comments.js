6a71d77a74c5d45c951307e76918849b4a288469
uuuuj <yjth4436@gmail.com>
2023-11-04 15:05:15 +0900

    feat: 댓글 목록 조회, 댓글 작성 API 구현, comment table 구현

---
+++ board/routes/comments.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Comment } = require("../models");
+const router = express.Router();
+
+// 댓글 조회 (해당 게시물의 모든 댓글): GET /posts/:postid/comments
+// 댓글 작성 (특정 게시물에 대한 새 댓글 추가): POST /posts/:postid/comments
+// 댓글 상세 조회: GET /comments/:commentid (이 경우에는 게시물 ID가 필요하지 않을 수 있습니다)
+// 댓글 수정: PUT /comments/:commentid
+// 댓글 삭제: DELETE /comments/:commentid
+
+router.get("/posts/:postid/comments", async (req, res) => {
+    const { postid } = req.params;
+
+    const comments = await Comment.findAll({
+        attributes: ["commentId", "writer", "content", "createdAt", "updatedAt"],
+        where: { postid }
+    });
+    res.status(200).json({ data : comments });
+
+});
+
+router.post("/posts/:postid/comments", async (req, res) => {
+    const { postid } = req.params;
+    const { writer, content } = req.body;
+    const comment = await Comment.create({ postid, writer, content });
+
+    res.status(201).json({ data: comment });
+});
+
+module.exports = router;
