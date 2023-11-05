bd21b876c530e8cf0e37b0f9d61d83507efb1e18
uuuuj <yjth4436@gmail.com>
2023-11-05 15:32:07 +0900

    임시 저장

--- board/app.js
+++ board/app.js
@@ -3,13 +3,14 @@ const app = express();
 const port = 3000;
 const postsRouter = require("./routes/posts");
 const commentsRouter = require("./routes/comments");
-const { sequelize } = require('./models/index.js');
 
-async function main() {
-    await sequelize.sync({ force: true });
-}
+// const { sequelize } = require('./models/index.js');
 
-main();
+// async function main() {
+//     await sequelize.sync({ force: true });
+// }
+
+// main();
 
 app.use(express.json());
 //loaclhost:3000/api -> postsRouter
